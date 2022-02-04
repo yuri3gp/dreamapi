f57675b24df95e75c763594398f3f25ed506d9c5
yuri3gp <yuri3gp@gmail.com>
2022-02-03 21:46:33 -0300

    KeywordController

---
+++ app/Http/Controllers/KeywordController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Keyword;
+use Illuminate\Http\Request;
+
+class KeywordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Keyword $keyword)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Keyword $keyword)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Keyword  $keyword
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Keyword $keyword)
+    {
+        //
+    }
+}

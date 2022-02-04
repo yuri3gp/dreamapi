78dc778c097eb95a4c77c708f1be059d21dd92c0
yuri3gp <yuri3gp@gmail.com>
2022-02-03 21:47:07 -0300

    User change

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,9 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function dreamkeyword()
-    {
-        return $this->belongsToMany(Dreamkeyword::class, 'dreamkeyword');
-    }
 }
